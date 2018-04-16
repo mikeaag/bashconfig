d78fd097ab4d009613914bc796cfd72d50bd5794
Mike Gatward <mike.gatward@xenonsoft.co.uk>
2018-04-16 23:33:57 +0100

    Add utilities and update install script

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@ cp .bashrc_settings ~/.bashrc_settings
 
 echo "configDir='$PWD/bashrc'" >> ~/.bashrc_settings
 echo "finishedLoading=''" >> ~/.bashrc_settings
-echo "debug=0" >> ~/.bashrc_settings
+echo "debug=1" >> ~/.bashrc_settings
+
+echo "Setup complete!"
+echo "Debug is on by default. Run `obrc` to edit it"
+
+sbrc
+
+h
